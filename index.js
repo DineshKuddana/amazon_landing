c175f4544b97df3a7094d9c7d6f9a8e5de01c821
Dinesh Kuddana <117835803+DineshKuddana@users.noreply.github.com>
2024-07-14 21:09:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let currentIndex = 0;
+
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slider-item');
+const totalSlides = slides.length;
+
+document.querySelector('.left-button').addEventListener('click', () => {
+    currentIndex = (currentIndex > 0) ? currentIndex - 1 : totalSlides - 1;
+    updateSlider();
+});
+
+document.querySelector('.right-button').addEventListener('click', () => {
+    currentIndex = (currentIndex < totalSlides - 1) ? currentIndex + 1 : 0;
+    updateSlider();
+});
+
+function updateSlider() {
+    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
